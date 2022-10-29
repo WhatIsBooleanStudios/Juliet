31f7c2a126f520551af91ec07ece7d6fe80723a2
theVerySharpFlat <aidenramen29@gmail.com>
2022-10-29 18:23:50 -0500

    preliminary window implementation

--- app/src/main/java/mclone/App.java
+++ app/src/main/java/mclone/App.java
@@ -3,18 +3,13 @@
  */
 package mclone;
 
+import mclone.platform.Window;
+
 import org.lwjgl.*;
-import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
-import org.lwjgl.system.*;
-
-import java.nio.*;
-
-import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryStack.*;
-import static org.lwjgl.system.MemoryUtil.*;
+
+import static org.lwjgl.opengl.GL33.*;
 
 public class App {
     public String getGreeting() {
@@ -22,7 +17,7 @@ public class App {
     }
 
     // The window handle
-    private long window;
+    private Window m_window;
 
     public void run() {
         System.out.println("Hello LWJGL " + Version.getVersion() + "!");
@@ -30,66 +25,13 @@ public class App {
         init();
         loop();
 
-        // Free the window callbacks and destroy the window
-        glfwFreeCallbacks(window);
-        glfwDestroyWindow(window);
-
-        // Terminate GLFW and free the error callback
-        glfwTerminate();
-        glfwSetErrorCallback(null).free();
+        Window.shutdownWindowSystem();
     }
 
     private void init() {
-        // Setup an error callback. The default implementation
-        // will print the error message in System.err.
-        GLFWErrorCallback.createPrint(System.err).set();
-
-        // Initialize GLFW. Most GLFW functions will not work before doing this.
-        if (!glfwInit())
-            throw new IllegalStateException("Unable to initialize GLFW");
-
-        // Configure GLFW
-        glfwDefaultWindowHints(); // optional, the current window hints are already the default
-        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
-
-        // Create the window
-        window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
-        if (window == NULL)
-            throw new RuntimeException("Failed to create the GLFW window");
-
-        // Setup a key callback. It will be called every time a key is pressed, repeated
-        // or released.
-        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-            if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
-                glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
-        });
-
-        // Get the thread stack and push a new frame
-        try (MemoryStack stack = stackPush()) {
-            IntBuffer pWidth = stack.mallocInt(1); // int*
-            IntBuffer pHeight = stack.mallocInt(1); // int*
-
-            // Get the window size passed to glfwCreateWindow
-            glfwGetWindowSize(window, pWidth, pHeight);
-
-            // Get the resolution of the primary monitor
-            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-
-            // Center the window
-            glfwSetWindowPos(
-                    window,
-                    (vidmode.width() - pWidth.get(0)) / 2,
-                    (vidmode.height() - pHeight.get(0)) / 2);
-        } // the stack frame is popped automatically
-
-        // Make the OpenGL context current
-        glfwMakeContextCurrent(window);
-        // Enable v-sync
-        glfwSwapInterval(1);
-
-        // Make the window visible
-        glfwShowWindow(window);
+        Window.initializeWindowSystem();
+        m_window = new Window("Window!", 720, 480, false);
+        m_window.makeContextCurrent();
     }
 
     private void loop() {
@@ -101,18 +43,29 @@ public class App {
         GL.createCapabilities();
 
         // Set the clear color
+        
         glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
 
         // Run the rendering loop until the user has attempted to close
         // the window or has pressed the ESCAPE key.
-        while (!glfwWindowShouldClose(window)) {
+        while (!m_window.shouldClose() && !m_window.keyPressed(GLFW_KEY_ESCAPE)) {
+            if(m_window.mouseButtonPressed(GLFW_MOUSE_BUTTON_LEFT)) {
+                glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
+            } else if(m_window.mouseButtonReleased(GLFW_MOUSE_BUTTON_LEFT)) {
+                glClearColor(1.0f, 1.0f, 0.0f, 1.0f);
+            } else {
+                glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+            }
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+            m_window.setTitle("Window! Cursor pos: " + m_window.getMousePosition().get(0) + " " + m_window.getMousePosition().get(1));
 
-            glfwSwapBuffers(window); // swap the color buffers
+            //glfwSwapBuffers(window); // swap the color buffers
+            m_window.swapBuffers();
 
             // Poll for window events. The key callback above will only be
             // invoked during this call.
-            glfwPollEvents();
+            //glfwPollEvents();
+            Window.windowSystemPollEvents();
         }
     }
 
