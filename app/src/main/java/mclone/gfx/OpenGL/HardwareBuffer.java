54ad36364b877b6162ded5cca19fedc643d18e21
theVerySharpFlat <aidenramen29@gmail.com>
2022-10-30 11:11:42 -0500

    add index buffers

--- app/src/main/java/mclone/gfx/OpenGL/HardwareBuffer.java
+++ app/src/main/java/mclone/gfx/OpenGL/HardwareBuffer.java
@@ -1,9 +1,25 @@
 package mclone.gfx.OpenGL;
 
+import static org.lwjgl.opengl.GL33C.*;
+
 public abstract class HardwareBuffer {
     enum UsageHints {
         USAGE_DYNAMIC,
         USAGE_STATIC
+    }
+
+    public static int usageHintToGLUsageHint(UsageHints hint) {
+        int GL_usageHint = 0;
+        switch (hint) {
+            case USAGE_DYNAMIC:
+                GL_usageHint = GL_DYNAMIC_DRAW;
+                break;
+            case USAGE_STATIC:
+                GL_usageHint = GL_STATIC_DRAW;
+                break;
+        }
+
+        return GL_usageHint;
     }
 
     public abstract void bind();
---
+++ app/src/main/java/mclone/gfx/OpenGL/IndexBuffer.java
@@ -0,0 +1,53 @@
+package mclone.gfx.OpenGL;
+
+import java.nio.ByteBuffer;
+
+import static org.lwjgl.opengl.GL33C.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+class IndexBuffer extends HardwareBuffer {
+
+	public IndexBuffer(ByteBuffer data, long size, UsageHints usage) {
+        m_ID = glGenBuffers();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ID);
+        int GL_usageHint = usageHintToGLUsageHint(usage);
+        if(data != null) {
+            nglBufferData(GL_ELEMENT_ARRAY_BUFFER, size, memAddress(data), GL_usageHint);
+        } else if(size > 0) {
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, size, GL_usageHint);
+        } else {
+            System.out.println("IndexBuffer: if data is null then size must be greater than 0");
+        }
+
+        m_maxSize = size;
+    }
+
+    public void setData(ByteBuffer data, long size) {
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ID);
+        nglBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0, size, memAddress(data));
+    }
+
+    @Override
+    public void bind() {
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ID);
+    }
+
+    @Override
+    public long getMaxSize() {
+        return m_maxSize;
+    }
+
+    @Override
+    public void unBind() {
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    }
+
+    @Override
+    public void dispose() {
+        glDeleteBuffers(m_ID);
+    }
+
+    private int m_ID = 0;
+    private long m_maxSize;
+
+}
