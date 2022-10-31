5ae7618e8a52912e54c357b39d7dc4598f03cd83
theVerySharpFlat <aidenramen29@gmail.com>
2022-10-31 11:34:11 -0500

    finished shader abstraction

--- app/src/main/java/mclone/gfx/OpenGL/Shader.java
+++ app/src/main/java/mclone/gfx/OpenGL/Shader.java
@@ -2,6 +2,8 @@ package mclone.gfx.OpenGL;
 
 import org.lwjgl.system.MemoryStack;
 
+import mclone.Logging.Logger;
+
 import java.nio.IntBuffer;
 
 import static org.lwjgl.opengl.GL33C.*;
@@ -13,14 +15,40 @@ public class Shader {
             m_fs = fs;
         }
 
-        String getVs() { return m_vs; }
-        String getFs() { return m_fs; }
+        public String getVS() { return m_vs; }
+        public String getFS() { return m_fs; }
 
-        String m_vs;
-        String m_fs;
+        private String m_vs;
+        private String m_fs;
     }
 
-    public int compileShader(int type, String src) {
+
+    public Shader(ShaderSource src) {
+        m_ID = glCreateProgram();
+        int vs = compileShader(GL_VERTEX_SHADER, src.getVS());
+        int fs = compileShader(GL_FRAGMENT_SHADER, src.getFS());
+
+        glAttachShader(m_ID, vs);
+        glAttachShader(m_ID, fs);
+        linkProgram(m_ID);
+
+        glDeleteShader(vs);
+        glDeleteShader(fs);
+    }
+
+    public void bind() {
+        glUseProgram(m_ID);
+    }
+
+    public void unBind() {
+        glUseProgram(0);
+    }
+
+    public void dispose() {
+        glDeleteProgram(m_ID);
+    }
+    
+    private int compileShader(int type, String src) {
         int shader = glCreateShader(type);
         glShaderSource(shader, src);
         glCompileShader(shader);
@@ -29,13 +57,27 @@ public class Shader {
             IntBuffer success = stack.mallocInt(1);
             glGetShaderiv(shader, GL_COMPILE_STATUS, success);
             if(success.get() <= 0) {
-                glGetShader
+                Logger.get().error("SHADER COMPILE ERROR: " + glGetShaderInfoLog(shader));
+                return -1;
+            } else {
+                return shader;
             }
         }
     }
 
-    public Shader(ShaderSource src) {
-        m_ID = glCreateProgram();
+    private boolean linkProgram(int program) {
+        glLinkProgram(program);
+
+        try(MemoryStack stack = MemoryStack.stackPush()) {
+            IntBuffer success = stack.mallocInt(1);
+            glGetProgramiv(program, GL_LINK_STATUS, success);
+            if(success.get() <= 0) {
+                Logger.get().error("SHADER LINK ERROR: " + glGetProgramInfoLog(program));
+                return false;
+            } else {
+                return true;
+            }
+        }
     }
 
     int m_ID;
