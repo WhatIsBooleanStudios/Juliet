361880e8432ab20c85327957adbf1ed1070ddb3f
theVerySharpFlat <aidenramen29@gmail.com>
2022-11-28 21:38:23 -0600

    format GUIManager

--- app/src/main/java/mclone/GFX/Renderer/GUIManager.java
+++ app/src/main/java/mclone/GFX/Renderer/GUIManager.java
@@ -19,53 +19,60 @@ import static org.lwjgl.glfw.GLFW.*;
  */
 
 class _ImColor {
-        public _ImColor(float R, float G, float B) {
-                r = R;
-                g = G;
-                b = B;
-        }
-        public _ImColor(float R, float G, float B, float A) {
-                r = R;
-                g = G;
-                b = B;
-                a = A;
-        }
-        public _ImColor(ImVec4 color) {
-                r = color.x * 255.0f;
-                g = color.y * 255.0f;
-                b = color.z * 255.0f;
-                a = color.w * 255.0f;
-        }
-        public ImVec4 getColor() {
-                return new ImVec4(r, g, b, a);
-        }
-        public float getRed() {
-                return r;
-        }
-        public float getGreen() {
-                return g;
-        }
-        public float getBlue() {
-                return b;
-        }
-        public float getAlpha() {
-                return a;
-        }
+    public _ImColor(float R, float G, float B) {
+        r = R;
+        g = G;
+        b = B;
+    }
 
-        private float r, g, b, a = 255;// these values will be in RGB
+    public _ImColor(float R, float G, float B, float A) {
+        r = R;
+        g = G;
+        b = B;
+        a = A;
+    }
+
+    public _ImColor(ImVec4 color) {
+        r = color.x * 255.0f;
+        g = color.y * 255.0f;
+        b = color.z * 255.0f;
+        a = color.w * 255.0f;
+    }
+
+    public ImVec4 getColor() {
+        return new ImVec4(r, g, b, a);
+    }
+
+    public float getRed() {
+        return r;
+    }
+
+    public float getGreen() {
+        return g;
+    }
+
+    public float getBlue() {
+        return b;
+    }
+
+    public float getAlpha() {
+        return a;
+    }
+
+    private float r, g, b, a = 255;// these values will be in RGB
 }
 
 public class GUIManager {
     protected GUIManager(Window window) {
         this.window = window.getNativeHandle();
     }
-    
+
     private void setImGuiStyleColor(final int colorName, final _ImColor colorValue) {
         ImGui.getStyle().setColor(colorName,
-        (float)colorValue.getRed() / 255.0f,
-        (float)colorValue.getGreen() / 255.0f,
-        (float)colorValue.getBlue()/255.0f,
-        (float)colorValue.getAlpha()/255.0f);
+            (float) colorValue.getRed() / 255.0f,
+            (float) colorValue.getGreen() / 255.0f,
+            (float) colorValue.getBlue() / 255.0f,
+            (float) colorValue.getAlpha() / 255.0f);
     }
 
     protected void init() {
@@ -82,46 +89,46 @@ public class GUIManager {
         setImGuiStyleColor(ImGuiCol.WindowBg, new _ImColor(37, 37, 38));
         setImGuiStyleColor(ImGuiCol.ChildBg, new _ImColor(37, 37, 38));
         setImGuiStyleColor(ImGuiCol.PopupBg, new _ImColor(37, 37, 38));
-        setImGuiStyleColor(ImGuiCol.Border, new _ImColor(78,78,78));
-        setImGuiStyleColor(ImGuiCol.BorderShadow, new _ImColor(78,78,78));
+        setImGuiStyleColor(ImGuiCol.Border, new _ImColor(78, 78, 78));
+        setImGuiStyleColor(ImGuiCol.BorderShadow, new _ImColor(78, 78, 78));
         setImGuiStyleColor(ImGuiCol.FrameBg, new _ImColor(51, 51, 55));
         setImGuiStyleColor(ImGuiCol.FrameBgHovered, new _ImColor(29, 151, 236));
-        setImGuiStyleColor(ImGuiCol.FrameBgActive, new _ImColor(0,119,200));
-        setImGuiStyleColor(ImGuiCol.TitleBg, new _ImColor(37,37,38));
-        setImGuiStyleColor(ImGuiCol.TitleBgActive, new _ImColor(37,37,38));
-        setImGuiStyleColor(ImGuiCol.TitleBgCollapsed, new _ImColor(37,37,38));
-        setImGuiStyleColor(ImGuiCol.MenuBarBg, new _ImColor(51,51,55));
-        setImGuiStyleColor(ImGuiCol.ScrollbarBg, new _ImColor(51,51,55));
-        setImGuiStyleColor(ImGuiCol.ScrollbarGrab, new _ImColor(82,82,85));
-        setImGuiStyleColor(ImGuiCol.ScrollbarGrabHovered, new _ImColor(90,90,95));
-        setImGuiStyleColor(ImGuiCol.ScrollbarGrabActive, new _ImColor(90,90,95));
+        setImGuiStyleColor(ImGuiCol.FrameBgActive, new _ImColor(0, 119, 200));
+        setImGuiStyleColor(ImGuiCol.TitleBg, new _ImColor(37, 37, 38));
+        setImGuiStyleColor(ImGuiCol.TitleBgActive, new _ImColor(37, 37, 38));
+        setImGuiStyleColor(ImGuiCol.TitleBgCollapsed, new _ImColor(37, 37, 38));
+        setImGuiStyleColor(ImGuiCol.MenuBarBg, new _ImColor(51, 51, 55));
+        setImGuiStyleColor(ImGuiCol.ScrollbarBg, new _ImColor(51, 51, 55));
+        setImGuiStyleColor(ImGuiCol.ScrollbarGrab, new _ImColor(82, 82, 85));
+        setImGuiStyleColor(ImGuiCol.ScrollbarGrabHovered, new _ImColor(90, 90, 95));
+        setImGuiStyleColor(ImGuiCol.ScrollbarGrabActive, new _ImColor(90, 90, 95));
         setImGuiStyleColor(ImGuiCol.CheckMark, new _ImColor(0, 119, 200));
         setImGuiStyleColor(ImGuiCol.SliderGrab, new _ImColor(29, 151, 236));
-        setImGuiStyleColor(ImGuiCol.SliderGrabActive, new _ImColor(0,119,200));
+        setImGuiStyleColor(ImGuiCol.SliderGrabActive, new _ImColor(0, 119, 200));
         setImGuiStyleColor(ImGuiCol.Button, new _ImColor(51, 51, 55));
         setImGuiStyleColor(ImGuiCol.ButtonHovered, new _ImColor(29, 151, 236));
         setImGuiStyleColor(ImGuiCol.ButtonActive, new _ImColor(29, 151, 236));
-        setImGuiStyleColor(ImGuiCol.Header, new _ImColor(51, 51,55));
-        setImGuiStyleColor(ImGuiCol.HeaderHovered, new _ImColor(29,151,236));
-        setImGuiStyleColor(ImGuiCol.HeaderActive, new _ImColor(0,119,200));
-        setImGuiStyleColor(ImGuiCol.Separator, new _ImColor(78,78,78));
-        setImGuiStyleColor(ImGuiCol.SeparatorHovered, new _ImColor(78,78,78));
-        setImGuiStyleColor(ImGuiCol.SeparatorActive, new _ImColor(78,78,78));
+        setImGuiStyleColor(ImGuiCol.Header, new _ImColor(51, 51, 55));
+        setImGuiStyleColor(ImGuiCol.HeaderHovered, new _ImColor(29, 151, 236));
+        setImGuiStyleColor(ImGuiCol.HeaderActive, new _ImColor(0, 119, 200));
+        setImGuiStyleColor(ImGuiCol.Separator, new _ImColor(78, 78, 78));
+        setImGuiStyleColor(ImGuiCol.SeparatorHovered, new _ImColor(78, 78, 78));
+        setImGuiStyleColor(ImGuiCol.SeparatorActive, new _ImColor(78, 78, 78));
         setImGuiStyleColor(ImGuiCol.ResizeGrip, new _ImColor(37, 37, 38));
-        setImGuiStyleColor(ImGuiCol.ResizeGripActive, new _ImColor(82,82,85));
+        setImGuiStyleColor(ImGuiCol.ResizeGripActive, new _ImColor(82, 82, 85));
         setImGuiStyleColor(ImGuiCol.ResizeGripHovered, new _ImColor(51, 51, 55));
-        setImGuiStyleColor(ImGuiCol.PlotLines, new _ImColor(0,119,200));
-        setImGuiStyleColor(ImGuiCol.PlotLinesHovered, new _ImColor(29,151,236));
-        setImGuiStyleColor(ImGuiCol.PlotHistogram, new _ImColor(0,119,200));
-        setImGuiStyleColor(ImGuiCol.PlotHistogramHovered, new _ImColor(29,151,236));
+        setImGuiStyleColor(ImGuiCol.PlotLines, new _ImColor(0, 119, 200));
+        setImGuiStyleColor(ImGuiCol.PlotLinesHovered, new _ImColor(29, 151, 236));
+        setImGuiStyleColor(ImGuiCol.PlotHistogram, new _ImColor(0, 119, 200));
+        setImGuiStyleColor(ImGuiCol.PlotHistogramHovered, new _ImColor(29, 151, 236));
         setImGuiStyleColor(ImGuiCol.ModalWindowDimBg, new _ImColor(37, 37, 37));
         setImGuiStyleColor(ImGuiCol.DragDropTarget, new _ImColor(37, 37, 37));
         setImGuiStyleColor(ImGuiCol.NavHighlight, new _ImColor(37, 37, 37));
-        setImGuiStyleColor(ImGuiCol.DockingPreview, new _ImColor(0,119,200));
+        setImGuiStyleColor(ImGuiCol.DockingPreview, new _ImColor(0, 119, 200));
         setImGuiStyleColor(ImGuiCol.Tab, new _ImColor(37, 37, 37));
-        setImGuiStyleColor(ImGuiCol.TabActive, new _ImColor(0,119,200));
-        setImGuiStyleColor(ImGuiCol.TabHovered, new _ImColor(37,37,37));
-        setImGuiStyleColor(ImGuiCol.TabUnfocused, new _ImColor(0,119,200));
+        setImGuiStyleColor(ImGuiCol.TabActive, new _ImColor(0, 119, 200));
+        setImGuiStyleColor(ImGuiCol.TabHovered, new _ImColor(37, 37, 37));
+        setImGuiStyleColor(ImGuiCol.TabUnfocused, new _ImColor(0, 119, 200));
         setImGuiStyleColor(ImGuiCol.TabUnfocusedActive, new _ImColor(29, 151, 236));
 
         glfwImpl.init(window, true);
